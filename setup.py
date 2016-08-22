c4f6109087768fda05885797d06b154d25880003
Camilo Sampedro <sampedro1903@gmail.com>
2016-08-22 13:52:05 -0500

    Configuring base files

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='Bleetanis',
+    package_dir={'', 'src'},
+    version='0.1',
+    author='Camilo Sampedro, Vanessa Perez',
+    author_email='camilo.sampedro@udea.edu.co, cindy.perez210@gmail.com',
+    url='https://github.com/camilosampedro/bleetanis',
+    description='Camera game',
+    classifiers=[
+        "Topic :: Games/Entertainment",
+        "Topic :: Games/Entertainment :: Arcade",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX :: Linux",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"
+    ]
+)
