2b461f205d46b4498e16a59b4462aefc97e55710
Camilo Sampedro <sampedro1903@gmail.com>
2016-08-24 16:20:44 -0500

    Corregido error inicial

--- src/prueba_camara03.py
+++ src/prueba_camara03.py
@@ -31,8 +31,12 @@ def join_images(image1, image2, x, y):
     image2_height = image2.shape[0]
     image2_width = image2.shape[1]
     # Align to center.
-    final_y = y - image2_width / 2 + image2_height
-    final_x = x - image2_height / 2 + image2_width
+    y = (y - image2_height / 2)
+    x = (x - image2_width / 2)
+    final_y = y + image2_height
+    final_x = x + image2_width
+    print("f_y: %s, f_x: %s" % (final_y, final_x))
+    print("s_y: %s, s_x: %s" % (image1.shape[0], image1.shape[1]))
     for c in range(0, 3):
         image1[y:final_y, x:final_x, c] = \
             image2[:, :, c] * (image2[:, :, 3]/255.0) + \
@@ -50,10 +54,9 @@ while True:
     img = cv2.GaussianBlur(np.copy(img_raw), (5, 5), 0)
     # Initialize last_x and last_y to center
     if (last_x == 0):
-        last_x = img.shape[0]/2
+        last_x = img.shape[1]/2
     if (last_y == 0):
-        last_y = img.shape[1]/2
-
+        last_y = img.shape[0]/2
     # img = cv2.fastNlMeansDenoisingColored(img, img, 1, 10)
     # define the list of boundaries)aries
     # boundaries = [
@@ -62,23 +65,19 @@ while True:
     #     ([25, 146, 190], [62, 174, 250]),
     #     ([103, 86, 65], [145, 133, 128])
     # ]
-
     # for (lower, upper) in boundaries:
     # Mask will contain areas that satisfy lower<pixel<upper
     mask = cv2.inRange(img, lower, upper)
     # Moments contain information about the mask areas
     moments = cv2.moments(mask)
-
     # Area will be the first moment
     area = moments['m00']
-
     # Check if area is big enough to be considered
     if(area > 90000):
-        # print('detected')
+        print('detected')
         # moments m10 and m01 will contain the found object "center"
         x = int(moments['m10']/area)
         y = int(moments['m01']/area)
-
         # Verify if there is a "big" movement since last checked
         if(abs(x-last_x) > 10 or abs(y-last_y) > 10):
             last_x = x
@@ -87,16 +86,17 @@ while True:
     else:
         print('not detected')
     # Always print penguin
+    print("last_x: %s, last_y: %s" % (last_x, last_y))
     join_images(img_raw, penguin_img, last_x, last_y)
 
     # Print pipes (normal and flipped) on both sides
-    join_images(img_raw, pipe_img, pipe_img.shape[1]-25, last_y)
+    join_images(img_raw, pipe_img, pipe_img.shape[1], last_y)
     join_images(img_raw, flipped_pipe,
-                img.shape[1]-pipe_img.shape[1]+25, last_y)
+                img.shape[1]-pipe_img.shape[1], last_y)
 
     # Print trampoline on the bottom
     join_images(img_raw, trampoline_img, last_x,
-                img.shape[0]-pipe_img.shape[0]-30)
+                img.shape[0]-pipe_img.shape[0])
 
     # Highlight areas found
     output = cv2.bitwise_and(img_raw, img_raw, mask=mask)
